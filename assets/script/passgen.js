b8b5a5abdb201615d1edb7dfc6699c4f3006f69b
jana <vs.janarthani@gmail.com>
2021-02-11 00:48:00 -0800

    added html and css codes

---
+++ assets/script/passgen.js
@@ -0,0 +1,29 @@
+// initialising variables
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+// Function to get random uppercase
+function uppperCase() {
+    return upper [Math.floor(Math.random() * upper.length)];  
+}
+console.log(uppperCase());
+
+// Function to get random lowercase
+function lowerCase() {
+    return lower [Math.floor(Math.random() * lower.length)];  
+}
+console.log(lowerCase());
+
+// Function to get random number
+function rdmNumber() {
+    return number [Math.floor(Math.random() * number.length)];  
+}
+console.log(rdmNumber());
+
+// Function to get random symbol
+function rdmSymbol() {
+    return symbol [Math.floor(Math.random() * symbol.length)];  
+}
+console.log(rdmSymbol());
