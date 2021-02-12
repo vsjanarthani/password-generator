5272d9a1ee893ac10bcdd59a52d2e5527e616f8b
jana <vs.janarthani@gmail.com>
2021-02-12 05:58:46 -0800

    added popup form and styling

--- assets/script/passgen.js
+++ assets/script/passgen.js
@@ -1,12 +1,39 @@
 // initialising variables
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lower = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", lower = "abcdefghijklmnopqrstuvwxyz", number = "0123456789", symbol = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
 
 // Function to display prompt when generate password button is clicked in HTML
 function promptDisplay() {
-    window.prompt ("Hi There!");
+    var element = document.getElementById("display");
+    element.classList.add("active");
+  }
+
+// //   Function to remove prompt after submit button is clicked
+// function removeDisplay() {
+//     var element = document.getElementById("display");
+//     element.classList.remove("active");
+//   }
+
+//   Function to check if passlen is >=8 and <=128
+function getInput() {
+    var passlength = document.getElementById("passLen").value;
+    passlength1= parseInt(passlength);
+    console.log(passlength1);
+    // console.log(typeof(passlength1));
+    var checkedUc = document.getElementById("uppercase").checked;
+    console.log(checkedUc);
+    var checkedLc = document.getElementById("lowercase").checked;
+    console.log(checkedLc);
+    var checkedNo = document.getElementById("numbers").checked;
+    console.log(checkedNo);
+    var checkedSl = document.getElementById("symbols").checked;
+    console.log(checkedSl);
+
+    // if (passlength1 < 8 || > 128) {
+    //   console.log ("bad choice");
+    // } else {
+    //     console.log("Your password will ge generated soon");
+    // }
+    // removeDisplay();
 }
 
 
